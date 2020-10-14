bb862fae499af545846e57a6dbc8f1bdb5f01810
karenfigueroa <70006519+karenfigueroa@users.noreply.github.com>
2020-10-14 15:11:44 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,25 @@
-from Bio.Seq import Seq 
 from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.SeqRecord import SeqRecord
 import os
 
+# Archivo .gbk de mi escritorio, cambiar a la dirección del nuevo archivo .gbk a leer
+filename =  "/mnt/c/Users/karen/Desktop/BIOINFORMATICA/biopython-notebook/notebooks/data/ls_orchid.gbk"
+
 def summarize_contents(filename):
-	record = SeqIO.read(filename, "genbank")
-	print("Name: ", record.name)
-	print("Path: ", os.path.dirname(filename))
-	records = list(SeqIO.parse (filename, "genbank"))
-	print("num_records = %i records" % len(records))
-	
-	for seq_record in SeqIO.parse(filename, "genbank"):
-		print("ID: ", record.id)
-		#Pendiente location
-	
+        all_records=[]
+        records = list(SeqIO.parse(filename, "genbank"))
+        print ("Path: ", os.path.dirname(filename))
+        print("num_records = %i records" % len(records))
+        print("\n\n")
+        for seq_record in SeqIO.parse(filename, "genbank"):
+                all_records.append(seq_record.name)
+                print("Name: ", seq_record.name)
+                print("ID:",seq_record.id)
+                print("Location:")
+                for seq_feature in seq_record.features :
+                        print('Start: %d, Stop: %d'%(int(seq_feature.location.start),int(seq_feature.location.end)))
+                print("\n")
+
 summarize_contents(filename)
