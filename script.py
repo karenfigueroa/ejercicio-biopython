c159f732d81507984aa16a2ef1944ea7e9d00f6e
karenfigueroa <70006519+karenfigueroa@users.noreply.github.com>
2020-10-14 15:18:49 -0700

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ import os
 # Archivo .gbk de mi escritorio, cambiar a la dirección del nuevo archivo .gbk a leer
 filename =  "/mnt/c/Users/karen/Desktop/BIOINFORMATICA/biopython-notebook/notebooks/data/ls_orchid.gbk"
 
+# Definición de la función summarize_contents
 def summarize_contents(filename):
         all_records=[]
         records = list(SeqIO.parse(filename, "genbank"))
@@ -18,8 +19,9 @@ def summarize_contents(filename):
                 print("Name: ", seq_record.name)
                 print("ID:",seq_record.id)
                 print("Location:")
-                for seq_feature in seq_record.features :
+                for seq_feature in seq_record.features:
                         print('Start: %d, Stop: %d'%(int(seq_feature.location.start),int(seq_feature.location.end)))
                 print("\n")
-
+        
+# Llamada a la función summarize_contents
 summarize_contents(filename)
