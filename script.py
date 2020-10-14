d437e5279b5549441fe15f55b604b396024171f5
karenfigueroa <70006519+karenfigueroa@users.noreply.github.com>
2020-10-14 10:48:46 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,18 @@
 from Bio.Seq import Seq 
-from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio import SeqIO
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.SeqRecord import SeqRecord
+import os
+
 def summarize_contents(filename):
 	record = SeqIO.read(filename, "genbank")
-	print(record)
+	print("Name: ", record.name)
+	print("Path: ", os.path.dirname(filename))
+	records = list(SeqIO.parse (filename, "genbank"))
+	print("num_records = %i records" % len(records))
+	
+	for seq_record in SeqIO.parse(filename, "genbank"):
+		print("ID: ", record.id)
+		#Pendiente location
+	
 summarize_contents(filename)
-
