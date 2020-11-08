63e1eb86b9065028e0057a6dfc2575cbfc3c4c37
karenfigueroa <karenfigueroae1@gmail.com>
2020-11-08 16:17:19 -0700

    Agrega nueva funcion a script.py Este commit agrega una función para generar el complemento inverso de la concatenación de dos cadenas de DNA, llamada concatenate_and_get_reverse_of_complement, al script.py. Para esto se utiliza la librería de biopython.
    [biopython-tarea2-ex1]

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import os
-
-filename =  os.path.abspath("data/ls_orchid.gbk")
 
 # Definición de la función summarize_contents
 def summarize_contents(filename):
@@ -31,7 +30,20 @@ def summarize_contents(filename):
 		diccionario['descriptions:'].append(seq_record.description)
 	return diccionario
 
+# Definición de la función concatenate_and_get_reverse_of_complement()
+def concatenate_and_get_reverse_of_complement(sec1, sec2):
+	# Concatena las 2 secuencias y las convierte en objeto Seq
+	concatenado = Seq(sec1 + sec2)
+	# Devuelve el reverso complementario en mayúsculas de la concatenación
+	return concatenado.reverse_complement().upper()
+
 # Llamada a la función summarize_contents
 if __name__ == "__main__":
+	filename =  os.path.abspath("data/ls_orchid.gbk")
+	secuencia_1 = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+	secuencia_2 = "AGTCTGGGATAGCTATCGTAA"
 	resultado = summarize_contents(filename)
-	print(resultado)
+	print("Resultado summarize_contents:\n", resultado)
+	resultado = concatenate_and_get_reverse_of_complement(secuencia_1, secuencia_2)
+	print("\nResultado concatenate_and_get_reverse_of_complement:\n", resultado)
+
